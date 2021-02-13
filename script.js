f5e9b247b6f4a635a5e186c1252e195c2e223e8b
Katrina Baha <kbaha@sandiego.edu>
2021-02-13 14:25:41 -0800

    added firebase

---
+++ script.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAKdkrS_djN7ZO0bo3S5QUCm8KYjc-juS8",
+    authDomain: "treehacksconnect.firebaseapp.com",
+    projectId: "treehacksconnect",
+    storageBucket: "treehacksconnect.appspot.com",
+    messagingSenderId: "617939153531",
+    appId: "1:617939153531:web:437c25c0c8d74dadf3e8de"
+  });
+  var db = firebase.firestore();
+  
